d97bc696caea803507cc66f78466d71364fc9236
Aditya Bhatt <adityabhatt@acm.org>
2011-07-12 04:05:34 +0530

    oops, add sqabs.c to git

---
+++ src/blocks/sqabs.c
@@ -0,0 +1,57 @@
+#ifndef __x86_64__
+#   define _FILE_OFFSET_BITS 64
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <inttypes.h>
+
+#include "sq_dsp.h"
+#include "sq_utils.h"
+
+char* usage_text[] = 
+{
+    "                                                                   ",
+    "NAME                                                               ",
+    "   sqabs - computes the absolute value of complex samples          ",
+    "SYNOPSIS                                                           ",
+    "   sqabs [OPTIONS] ...                                             ",
+    "DESCRIPTION                                                        ",
+    "   -l number of samples to read in one go.                         "
+};
+
+unsigned int smpls_len = 100000;
+
+int main(int argc, char **argv)
+{
+
+    int opt;
+
+    while ((opt = getopt(argc, argv, "hl:")) != -1)
+    {
+        switch (opt)
+        {
+            case 'h':
+                print_usage(usage_text);
+                exit(EXIT_FAILURE);
+            case 'l':
+                sscanf(optarg, "%u", &smpls_len);
+                break;
+            default:
+                print_usage(usage_text);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    int status = sq_abs(stdin, stdout, smpls_len);
+    
+    if(status < 0)
+    {
+        fprintf(stderr, "%s encountered a fatal error.", argv[0]);
+        sq_error_handle(status);
+        exit(EXIT_FAILURE);
+    }
+    
+    exit(EXIT_SUCCESS);
+}
