dc1684144c1914a494e27b63c9366437a281d4f0
Aditya Bhatt <aditya@aditya-laptop.localdomain>
2011-07-14 06:08:12 +0530

    Fix length option passing in sqcrosscorr and sqconvolution scripts

--- src/blocks/sqconvolution.sh
+++ src/blocks/sqconvolution.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 while getopts l: OPTION
-do
-    case $OPTION in 
+    do
+        case $OPTION in 
         l) length=$OPTARG;;
-        \?) print -u2 "Usage: $0 <file1> <file2> [ -l points ]"
-        exit 2;;
-    esac
+    \?) print -u2 "Usage: $0 [ -l points ] <file1> <file2>"
+    exit 2;;
+esac
 done
+
 mkfifo fft1
 mkfifo fft2
 
-length=4096
-
-cat $1 | sqsample -l $length | sqwindow -l $length | sqfft -l $length > fft1 &
-cat $2 | sqsample -l $length | sqwindow -l $length | sqfft -l $length > fft2 &
+cat $3 | sqsample -l $length | sqwindow -l $length | sqfft -l $length > fft1 &
+cat $4 | sqsample -l $length | sqwindow -l $length | sqfft -l $length > fft2 &
 
 sqcrossmultiply fft1 fft2 -l $length | sqfft -l $length -i
 
