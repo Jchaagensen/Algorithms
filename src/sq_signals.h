da36fb4e214c577fa6b68216bc5dd3941b095af6
Aditya Bhatt <adityabhatt@acm.org>
2011-07-22 09:58:50 +0530

    sq_signals dox++

--- src/sq_signals.h
+++ src/sq_signals.h
@@ -3,6 +3,15 @@
 
 #include <stdio.h>
 
+/**
+ * Generate a sinewave with gaussian noise 
+ * @param outstream output stream
+ * @param nsamples Number of samples to read/process/write in one go
+ * @param length Length of sinewave
+ * @param sin_arr_length Number of samples in one period
+ * @param wavelength Wavelength of sinewave
+ * @param SNR The signal-to-noise ratio
+ */
 int sq_gen_sine(FILE* outstream,
                 unsigned int nsamples,
                 unsigned int length,
