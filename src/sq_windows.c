c5f7a377d0aad87d79356727f26636e24e34ce15
Aditya Bhatt <adityabhatt@acm.org>
2011-07-23 17:52:35 +0000

    sq_windows.h and sq_windows.c modules added. These will contain windowing functions. Hann and Hamming windows included for now.

---
+++ src/sq_windows.c
@@ -0,0 +1,38 @@
+#include <math.h>
+#include <string.h>
+
+#include "sq_windows.h"
+#include "sq_constants.h"
+
+void make_window (float* window_buffer, unsigned int length, float (*window_function)(unsigned int, unsigned int))
+{
+    unsigned int n;
+    for (n = 0; n < length; n++)
+        window_buffer[n] = sq_window_hann(n, length);
+}
+
+int make_window_from_name ( float* window_buffer, unsigned int length, char* window_name )
+{
+    float (*window_function)(unsigned int, unsigned int);
+    
+    if(!strcmp(window_name, "hann"))
+        window_function = sq_window_hann;
+    else if(!strcmp(window_name, "hamming"))
+        window_function = sq_window_hamming;
+    else
+        return ERR_UNKNOWN_WINDOW;
+    
+    make_window(window_buffer, length, window_function);
+    return 0;
+}
+
+float sq_window_hann (unsigned int n, unsigned int length)
+{
+    return 0.5 * (1 - cos( (2.0*M_PI*n)/(length-1) ));
+}
+
+float sq_window_hamming ( unsigned int n, unsigned int length )
+{
+    return 0.54 - 0.46 * cos( (2.0*M_PI*n)/(length-1) );
+}
+
---
+++ src/sq_windows.h
@@ -0,0 +1,10 @@
+#ifndef SQ_WINDOWS_H
+#define SQ_WINDOWS_H
+
+void make_window(float* window_buffer, unsigned int length, float (*window_function)(unsigned int, unsigned int));
+int make_window_from_name( float* window_buffer, unsigned int length, char* window_name);
+
+float sq_window_hann(unsigned int n, unsigned int length);
+float sq_window_hamming(unsigned int n, unsigned int length);
+
+#endif
