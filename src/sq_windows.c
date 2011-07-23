fdb53bdb31b6ec409c7c1dabe947e5cdb2faa720
Aditya Bhatt <adityabhatt@acm.org>
2011-07-23 20:01:49 +0000

    * Fix use window_function function pointer instead of sq_window_hann in sq_make_window * Use double casting in window functions * Add cosine window, addressable as both 'sine' and 'cosine'

--- src/sq_windows.c
+++ src/sq_windows.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <string.h>
+#include <stdio.h>
 
 #include "sq_windows.h"
 #include "sq_constants.h"
@@ -8,17 +9,19 @@ void sq_make_window (float* window_buffer, unsigned int length, float (*window_f
 {
     unsigned int n;
     for (n = 0; n < length; n++)
-        window_buffer[n] = sq_window_hann(n, length);
+        window_buffer[n] = window_function(n, length);
 }
 
 int sq_make_window_from_name ( float* window_buffer, unsigned int length, char* window_name )
 {
     float (*window_function)(unsigned int, unsigned int);
-    
+        
     if(!strcmp(window_name, "hann"))
         window_function = sq_window_hann;
     else if(!strcmp(window_name, "hamming"))
         window_function = sq_window_hamming;
+    else if(!strcmp(window_name, "sine") || !strcmp(window_name, "cosine"))
+        window_function = sq_window_cosine;
     else
         return ERR_UNKNOWN_WINDOW;
     
@@ -28,11 +31,17 @@ int sq_make_window_from_name ( float* window_buffer, unsigned int length, char* 
 
 float sq_window_hann (unsigned int n, unsigned int length)
 {
-    return 0.5 * (1 - cos( (2.0*M_PI*n)/(length-1) ));
+    return 0.5 * (1 - cos( (double)(2.0*M_PI*n) / ((double)(length-1)) ));
 }
 
 float sq_window_hamming ( unsigned int n, unsigned int length )
 {
-    return 0.54 - 0.46 * cos( (2.0*M_PI*n)/(length-1) );
+    return 0.54 - 0.46 * cos( (double)(2.0*M_PI*n) / ((double)(length-1)) );
 }
+
+float sq_window_cosine ( unsigned int n, unsigned int length )
+{
+    return sin( M_PI*n/(length-1) );
+}
+
 
